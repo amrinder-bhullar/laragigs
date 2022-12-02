7320e5884e54db4eaf00fc6bf64c048281a114a0
Amrinder <keptanamrinder@gmail.com>
2022-12-02 09:39:59 +0000

    added views

--- resources/views/listing.blade.php
+++ resources/views/listing.blade.php
@@ -1,16 +1,50 @@
-<!DOCTYPE html>
-<html lang="en">
+<x-layout>
+    <x-search />
+    <a href="/" class="inline-block text-black ml-4 mb-4"><i class="fa-solid fa-arrow-left"></i> Back
+    </a>
+    <div class="mx-4">
+        <x-card>
+            <div class="flex flex-col items-center justify-center text-center">
+                <img class="w-48 mr-6 mb-6" src="{{ asset('images/no-image.png') }}" alt="" />
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
+                <h3 class="text-2xl mb-2">{{ $listing->title }}</h3>
+                <div class="text-xl font-bold mb-4">{{ $listing->company }}</div>
+                <ul class="flex">
+                    @php
+                        $tags = explode(',', $listing->tags);
+                    @endphp
+                    @foreach ($tags as $tag)
+                        <li class="bg-black text-white rounded-xl px-3 py-1 mr-2">
+                            <a href="#">{{ $tag }}</a>
+                        </li>
+                    @endforeach
+                </ul>
+                <div class="text-lg my-4">
+                    <i class="fa-solid fa-location-dot"></i> {{ $listing->location }}
+                </div>
+                <div class="border border-gray-200 w-full mb-6"></div>
+                <div>
+                    <h3 class="text-3xl font-bold mb-4">
+                        Job Description
+                    </h3>
+                    <div class="text-lg space-y-6">
+                        <p>
+                            {{ $listing->description }}
+                        </p>
+                        <div class="mt-4">
+                            <a href="mailto:{{ $listing->email }}"
+                                class=" px-4 bg-laravel text-white mt-6 py-2 rounded-xl hover:opacity-80 max-w-sm"><i
+                                    class="fa-solid fa-envelope"></i>
+                                Contact Employer</a>
 
-<body>
-    <h1>{{ $listing->title }}</h1>
-    <p>{{ $listing->description }}</p>
-</body>
-
-</html>
+                            <a href="{{ $listing->website }}" target="_blank"
+                                class="px-4 bg-black text-white py-2 rounded-xl hover:opacity-80 max-w-sm"><i
+                                    class="fa-solid fa-globe"></i> Visit
+                                Website</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </x-card>
+    </div>
+</x-layout>
--- resources/views/listings.blade.php
+++ resources/views/listings.blade.php
@@ -1,20 +1,9 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Jobs Listings</title>
-</head>
-
-<body>
-    <h1>Jobs Listings</h1>
-    @foreach ($listings as $listing)
-        <a href="/listings/{{ $listing->id }}">
-            <h3>{{ $listing->title }}</h3>
-        </a>
-    @endforeach
-</body>
-
-</html>
+<x-layout>
+    <x-hero />
+    <x-search />
+    <div class="lg:grid lg:grid-cols-2 gap-4 space-y-4 md:space-y-0 mx-4">
+        @foreach ($listings as $listing)
+            <x-listing-card :listing="$listing" />
+        @endforeach
+    </div>
+</x-layout>
